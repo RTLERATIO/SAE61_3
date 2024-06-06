07edddaccc275abba23a1138c696b39f9ab2d72e
RTLERATIO <126696545+RTLERATIO@users.noreply.github.com>
2024-06-06 16:44:47 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 echo "Démarrage des conteneurs en cours..."
 docker-compose up --build
 sleep 10
-host=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' mariadb)
+host=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' sae61_3_php_1)
 mysql -u root -p'foo' -h $host < ~/SAE61_61/data.sql
 
 echo "Les conteneurs on été déployés !"
