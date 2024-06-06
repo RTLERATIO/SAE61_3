183e9e591a9c53236f840f62e6276470c0967fef
RTLERATIO <126696545+RTLERATIO@users.noreply.github.com>
2024-06-06 16:38:41 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,7 @@
+echo "Démarrage des conteneurs en cours..."
 docker-compose up --build
-echo "Démarrage des conteneurs..."
 sleep 10
-#Création de la table Utilisateurs
-#echo "Récupération de l'IP du conteneur MariaDB et insertion de la table Utilisateurs..."
-
 host=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' mariadb)
-mysql -u root -p'foo' -h $host < ~/data.sql
+mysql -u root -p'foo' -h $host < ~/SAE61_61/data.sql
 
-echo "OK !"
+echo "Les conteneurs on été déployés !"
