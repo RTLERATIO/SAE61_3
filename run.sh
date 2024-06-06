5d4c953771d3ef3dadaaec26e9b7015017d5498a
RTLERATIO <126696545+RTLERATIO@users.noreply.github.com>
2024-06-06 17:55:39 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 echo "Démarrage des conteneurs en cours..."
 docker-compose up --build -d
 sleep 10
-host=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' sae61_3_db_1)
+host1=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' sae61_3_db_1)
+host2=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' sae61_3_php_1)
 echo "Les conteneurs on été déployés !"
-echo "Voici l'IP du site : $host"
-mysql -u user -p'user' -h $host < data.sql
+echo "Voici l'IP du site : $host2"
+mysql -u user -p'user' -h $host1 < data.sql
