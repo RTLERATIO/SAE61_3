6ccb345524f457af17fff812d563ca15c09d81d6
RTLERATIO <126696545+RTLERATIO@users.noreply.github.com>
2024-06-06 18:39:08 +0200

    Update data.sql

--- data.sql
+++ data.sql
@@ -1,16 +1,7 @@
-CREATE DATABASE IF NOT EXISTS `test`;
-
-CREATE USER 'user'@'localhost' IDENTIFIED BY 'user';
-GRANT ALL PRIVILEGES ON test.* TO 'user'@'localhost';
-FLUSH PRIVILEGES;
-
-USE test;
-
-CREATE TABLE user (
+CREATE TABLE IF NOT EXISTS test.user (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) NOT NULL,
     adressemail VARCHAR(100) NOT NULL,
     password VARCHAR(255) NOT NULL,
     salt VARCHAR(255) NOT NULL
 );
-
