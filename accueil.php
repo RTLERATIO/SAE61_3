fe3cae3db1eb4b391ce76239f388d9c9b14015e2
RTLERATIO <126696545+RTLERATIO@users.noreply.github.com>
2024-06-07 09:10:13 +0200

    Update accueil.php

--- accueil.php
+++ accueil.php
@@ -4,10 +4,8 @@ ini_set('display_errors', 1);
 
 session_start();
 
-// Vérifiez si le nom d'utilisateur est défini dans la session
 $usernamesave = isset($_SESSION['username']) ? $_SESSION['username'] : '';
 
-// Redirigez vers la page de connexion si le nom d'utilisateur n'est pas défini
 if (empty($usernamesave)) {
     header('Location: index.php');
     exit();
@@ -20,7 +18,6 @@ if (empty($usernamesave)) {
     <meta charset="UTF-8">
     <title>Accueil SAE61</title>
     <style>
-        /* Ajoutez vos styles CSS ici */
         body {
             font-family: Arial, sans-serif;
             background-color: #f2f2f2;
@@ -34,9 +31,6 @@ if (empty($usernamesave)) {
             padding: 10px;
         }
 
-        .headerbottom {
-            /* Ajoutez les styles de l'en-tête inférieur ici */
-        }
 
         .pagecontent {
             padding: 20px;
@@ -63,23 +57,18 @@ if (empty($usernamesave)) {
             <img class="cookie-img" src="https://img.freepik.com/photos-gratuite/biscuits-aux-pepites-chocolat-isoles-fond-blanc-ai-generatif_123827-24066.jpg">
 
             <?php
-            // Connexion à la base de données
             $conn = new mysqli("db", "user", "user", "test");
 
-            // Vérifiez si la connexion a échoué
             if ($conn->connect_error) {
                 die("Erreur de connexion à la base de données : " . $conn->connect_error);
             }
 
-            // Requête SQL pour récupérer tous les utilisateurs
             $sql = "SELECT * FROM user";
             $result = $conn->query($sql);
 
-            // Vérifiez s'il y a des résultats
             if ($result->num_rows > 0) {
                 echo "<br>Voici la liste des utilisateurs : <br><br>";
                 echo "<ul>";
-                // Parcourez les résultats et affichez chaque utilisateur
                 while ($row = $result->fetch_assoc()) {
                     echo "<li>" . $row["username"] . "</li>";
                 }
@@ -87,8 +76,6 @@ if (empty($usernamesave)) {
             } else {
                 echo "Aucun utilisateur trouvé.";
             }
-
-            // Fermez la connexion à la base de données
             $conn->close();
             ?>
         </span></td></tr>
